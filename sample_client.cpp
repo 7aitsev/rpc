2bab648be7c6d3ff378142b880a9b9d3a21c3971
Maxim Zaitsev <7aitsev@pm.me>
2019-04-30 23:20:59 +0300

    adds function to compute progression
    adds include as it was removed from client.hpp
    cerr instead of cout
    removes trailing whitespaces on empty lines

--- sample_client.cpp
+++ sample_client.cpp
@@ -1,42 +1,49 @@
 #include "client.hpp"
 #include "json_rpc.hpp"
 
+#include <iostream>
+
 using namespace rpc;
 
-double compute_ap(const char * port, int b, int e) {
+double compute_ap_local(int begin, int end) {
+    return ((begin + end) * (end - begin + 1)) / 2.0;
+}
+
+double compute_ap_remote(const char * port, int b, int e) {
     client<json::json_rpc> client(port);
     double res = 0;
-    
+
     try {
         for(int i = b; i <= e; ++i) {
             res = client.call("add", {res, double(i)});
             std::cout << i << ": res = " << res << '\n';
         }
     } catch (const std::exception & e) {
-        std::cout << e.what() << '\n';
+        std::cerr << e.what() << '\n';
         return 1;
     }
-    
+
     return res;
 }
 
 int main(int argc, char ** argv) {
-    if(argc != 2) {
-        std::cout << "Usage: sample_client <port>\n";
+    if(2 != argc) {
+        std::cerr << "Usage: sample_client <port>\n";
         return 1;
     }
-    
+
     auto task1 = std::async(std::launch::async,
-            compute_ap, argv[1], 0, 10);
+            compute_ap_remote, argv[1], 0, 10);
     auto task2 = std::async(std::launch::async,
-            compute_ap, argv[1], 10, 20);
-    
+            compute_ap_remote, argv[1], 10, 20);
+
     double res1 = task1.get();
     double res2 = task2.get();
-    
-    std::cout << "\n===== Summary =====\n"
-              << "First:  " << res1 << " == " << 10.0*11/2 << '\n'
-              << "Second: " << res2 << " == " << (10.0+20)*11/2 << '\n';
-    
+
+    std::cout
+        << "\n===== Summary =====\n"
+        << "First:  " << res1 << " == " << compute_ap_local(0,  10) << '\n'
+        << "Second: " << res2 << " == " << compute_ap_local(10, 20) << '\n';
+
     return 0;
 }
