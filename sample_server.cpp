020f0686bd097b13a01304e777e137ba89e9873c
Maxim Zaitsev <7aitsev@pm.me>
2019-04-30 23:22:53 +0300

    cerr instead of cout
    removes trailing whitespaces on empty lines

--- sample_server.cpp
+++ sample_server.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
 #include "json_rpc.hpp"
 #include "server.hpp"
+
+#include <iostream>
 
 using namespace rpc;
 
@@ -13,27 +14,27 @@ struct div {
 } div_ftr;
 
 int main(int argc, char ** argv) {
-    if(argc != 2) {
-        std::cout << "Usage: server <port>\n";
+    if(2 != argc) {
+        std::cerr << "Usage: server <port>\n";
         return 1;
     }
-    
+
     server<json::json_rpc> server(argv[1]);
-    
+
     server.bind("sub", [](double a, double b) { return a - b; });
-    
+
     std::function<double (double, double)> add_obj = add;
     server.bind("add", add_obj);
-    
+
     server.bind("div", div_ftr);
-    
+
     try {
         server.run(2);
     } catch (const std::exception & e) {
-        std::cout << "Server failed: " << e.what() << "\n";
+        std::cerr << "Server failed: " << e.what() << "\n";
         return 1;
     }
-    
+
     std::cout << "Finished\n";
     return 0;
 }
